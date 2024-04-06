819435e50330be1247c1d42971eefc9b323fde3c
copyandbuild <153541835+copyandbuild@users.noreply.github.com>
2024-04-06 02:29:31 +0200

    Fixed Website

---
+++ script.js
@@ -0,0 +1,16 @@
+// Smooth scroll to sections when clicking on navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+  
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+  
